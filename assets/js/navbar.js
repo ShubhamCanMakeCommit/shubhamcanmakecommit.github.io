e71cfb75396d3aa276efcfe8671f4b70425cba15
Shubham Shah <shubhamshahrising@gmail.com>
2021-06-21 11:13:14 +0530

    Fix current link on navbar

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,7 +1,11 @@
-const NavbarLinks = document.querySelectorAll('.navbar__link')
+const NavbarLinks = document.querySelectorAll(".navbar__link");
 
 NavbarLinks.forEach((link) => {
-  if (location.href.endsWith(link.href)) {
-    link.classList.add('navbar__link--current')
+  if (
+    location.href.endsWith(link.href) ||
+    location.href.endsWith(link.href + "/")
+  ) {
+    link.classList.add("navbar__link--current");
+    console.log("added to", link.href);
   }
-})
+});
